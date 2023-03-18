27fd610e1e52b5cd94f216a1ad2a05f564be1b93
Lostor <102790212+Lstrzz@users.noreply.github.com>
2023-03-18 19:35:11 +0700

    Добавлены окна авторизации и Восстановления пароля

---
+++ Personnel/LoginWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Personnel
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow : Window
+    {
+        bool Ch = false;
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+            passwordTxtBox.Text = PasswordBox.Password;
+            PasswordBox.Visibility = Visibility.Collapsed;
+            passwordTxtBox.Visibility = Visibility.Visible;
+            Ch = true;
+        }
+
+        private void CheckBox_Unchecked(object sender, RoutedEventArgs e)
+        {
+            PasswordBox.Password = passwordTxtBox.Text;
+            passwordTxtBox.Visibility = Visibility.Collapsed;
+            PasswordBox.Visibility = Visibility.Visible;
+            Ch = false;
+        }
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            MailRecoveryWindow mailRecoveryWindow = new MailRecoveryWindow();
+            mailRecoveryWindow.Show();
+            this.Close();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Personnel/MailRecoveryWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Personnel
+{
+    /// <summary>
+    /// Логика взаимодействия для MailRecoveryWindow.xaml
+    /// </summary>
+    public partial class MailRecoveryWindow : Window
+    {
+        public MailRecoveryWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MailSP.Visibility = Visibility.Collapsed;
+            CodeSP.Visibility = Visibility.Visible;
+        }
+
+        private void Hyperlink_Click(object sender, RoutedEventArgs e)
+        {
+            if(CodeSP.Visibility == Visibility.Visible)
+            {
+                MailSP.Visibility = Visibility.Visible;
+                CodeSP.Visibility = Visibility.Collapsed;
+            }
+            else
+            {
+                LoginWindow loginWindow = new LoginWindow();
+                loginWindow.Show();
+                this.Close();
+            }
+        }
+    }
+}
